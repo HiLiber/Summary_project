73d92b5bd4a19fb7e08fc2f93af93617a50df5c8
HiLiber <henny90974@gmail.com>
2022-05-04 15:19:25 +0300

    lll

--- server/server.js
+++ server/server.js
@@ -1,47 +1,29 @@
 const exp = require('express')
-const { json } = require('express/lib/response')
-const app = exp()
-
-app.use(exp.urlencoded({extended:true}))
-app.use(exp.json())
-
-app.set('view engine', 'ejs')
 
 const Net = require('net');
 
 const port = 3000;
 
 const server = new Net.Server();
-server.listen(port, function() {
+server.listen(port, function () {
     console.log(`Server listening for connection requests on socket localhost:${port}.`);
 });
 
-server.on('connection', function(socket) {
+server.on('connection', function (socket) {
     console.log('A new connection has been established.');
 
     socket.write('Hello, client.');
 
-    socket.on('data', function(chunk) {
+    socket.on('data', function (chunk) {
         console.log(`Data received from client: ${chunk.toString()}.`);
         console.log(chunk.toJSON());
     });
-   
-    socket.on('close', function() {
+
+    socket.on('close', function () {
         console.log('Closing connection with the client');
     });
-   
-    socket.on('error', function(err) {
+
+    socket.on('error', function (err) {
         console.log(`innnn Error: ${err}`);
     });
 });
-
-app.get('/',(req,res)=>{
-    res.send('hhhhhhhhhh')
-})
-
-app.post('/sendMessage', (req, res) => {
-    const data = req.body;
-    res.render('page', {data:data})
-// res.send('hello')
-
-})
